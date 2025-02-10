64e789d84c76a8f0ea250331d98e0ae416cb78d0
confidential-nt <youyoy689@gmail.com>
2025-02-10 21:33:21 +0900

    test: 반복 규칙 기능 관련 테스트 추가

--- src/__tests__/repeat.spec.tsx
+++ src/__tests__/repeat.spec.tsx
@@ -48,20 +48,155 @@ describe.only('반복 유형 선택', () => {
       (option) => option.textContent
     );
 
-    const expectedOptions = ['29일', '5번째 목요일', '마지막 목요일', '미자막 날'];
+    const expectedOptions = [
+      '매월 29일',
+      '매월 5번째 목요일',
+      '매월 마지막 목요일',
+      '매월 마지막 날',
+    ];
 
     expectedOptions.forEach((option) => {
       expect(repeatRuleOptions).toContain(option);
     });
   });
 
-  it('윤년의 2월 29일에 매년 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매년 2월 29일, 해당 연도의 2월에서 29일이 속한 주차의 같은 요일, 해당 연도의 2월 마지막 동일 요일, 해당 연도의 2월 마지막 날.', () => {});
+  it('윤년의 2월 29일에 매년 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매년 2월 29일, 해당 연도의 2월에서 29일이 속한 주차의 같은 요일, 해당 연도의 2월 마지막 동일 요일, 해당 연도의 2월 마지막 날.', async () => {
+    const { user } = setup(<App />);
 
-  it('31일에 매월 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매월 31일, 해당 월에서 31일이 속한 주차의 같은 요일, 해당 월의 마지막 동일 요일, 해당 월의 마지막 날.', () => {});
+    await user.type(screen.getByLabelText('날짜'), '2024-02-29');
 
-  it('31일에 매년 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매년 동일 월의 31일, 해당 연도의 동일 월에서 31일이 속한 주차의 같은 요일, 해당 연도의 동일 월 마지막 동일 요일, 해당 연도의 동일 월 마지막 날.', () => {});
+    const checkbox = screen.getByLabelText('반복 일정') as HTMLInputElement;
+    if (!checkbox.checked) {
+      await user.click(checkbox);
+    }
 
-  it('그외 날짜에 매월 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매월 o일, 해당 월에서 o일이 속한 주차의 같은 요일', () => {});
+    const repeatTypeSelect = screen.getByLabelText('반복 유형');
+    await user.selectOptions(repeatTypeSelect, '매년');
 
-  it('그외 날짜에 매년 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매년 o월 o일, 해당 연도의 o월에서 o일이 속한 주차의 같은 요일', () => {});
+    const repeatRuleSelect = (await screen.findByLabelText('반복 규칙')) as HTMLSelectElement;
+    const repeatRuleOptions = Array.from(repeatRuleSelect.options).map(
+      (option) => option.textContent
+    );
+
+    const expectedOptions = [
+      '매년 2월 29일',
+      '매년 2월 5번째 목요일',
+      '매년 2월 마지막 목요일',
+      '매년 2월 마지막 날',
+    ];
+
+    expectedOptions.forEach((option) => {
+      expect(repeatRuleOptions).toContain(option);
+    });
+  });
+
+  it('31일에 매월 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매월 31일, 해당 월에서 31일이 속한 주차의 같은 요일, 해당 월의 마지막 동일 요일, 해당 월의 마지막 날.', async () => {
+    const { user } = setup(<App />);
+
+    await user.type(screen.getByLabelText('날짜'), '2024-03-31');
+
+    const checkbox = screen.getByLabelText('반복 일정') as HTMLInputElement;
+    if (!checkbox.checked) {
+      await user.click(checkbox);
+    }
+
+    const repeatTypeSelect = screen.getByLabelText('반복 유형');
+    await user.selectOptions(repeatTypeSelect, '매월');
+
+    const repeatRuleSelect = (await screen.findByLabelText('반복 규칙')) as HTMLSelectElement;
+    const repeatRuleOptions = Array.from(repeatRuleSelect.options).map(
+      (option) => option.textContent
+    );
+
+    const expectedOptions = [
+      '매월 31일',
+      '매월 5번째 일요일',
+      '매월 마지막 일요일',
+      '매월 마지막 날',
+    ];
+
+    expectedOptions.forEach((option) => {
+      expect(repeatRuleOptions).toContain(option);
+    });
+  });
+
+  it('31일에 매년 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매년 동일 월의 31일, 해당 연도의 동일 월에서 31일이 속한 주차의 같은 요일, 해당 연도의 동일 월 마지막 동일 요일, 해당 연도의 동일 월 마지막 날.', async () => {
+    const { user } = setup(<App />);
+
+    await user.type(screen.getByLabelText('날짜'), '2024-03-31');
+
+    const checkbox = screen.getByLabelText('반복 일정') as HTMLInputElement;
+    if (!checkbox.checked) {
+      await user.click(checkbox);
+    }
+
+    const repeatTypeSelect = screen.getByLabelText('반복 유형');
+    await user.selectOptions(repeatTypeSelect, '매년');
+
+    const repeatRuleSelect = (await screen.findByLabelText('반복 규칙')) as HTMLSelectElement;
+    const repeatRuleOptions = Array.from(repeatRuleSelect.options).map(
+      (option) => option.textContent
+    );
+
+    const expectedOptions = [
+      '매년 3월 31일',
+      '매년 3월 5번째 일요일',
+      '매년 3월 마지막 일요일',
+      '매년 3월 마지막 날',
+    ];
+
+    expectedOptions.forEach((option) => {
+      expect(repeatRuleOptions).toContain(option);
+    });
+  });
+
+  it('그외 날짜에 매월 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매월 o일, 해당 월에서 o일이 속한 주차의 같은 요일', async () => {
+    const { user } = setup(<App />);
+
+    await user.type(screen.getByLabelText('날짜'), '2024-02-17');
+
+    const checkbox = screen.getByLabelText('반복 일정') as HTMLInputElement;
+    if (!checkbox.checked) {
+      await user.click(checkbox);
+    }
+
+    const repeatTypeSelect = screen.getByLabelText('반복 유형');
+    await user.selectOptions(repeatTypeSelect, '매월');
+
+    const repeatRuleSelect = (await screen.findByLabelText('반복 규칙')) as HTMLSelectElement;
+    const repeatRuleOptions = Array.from(repeatRuleSelect.options).map(
+      (option) => option.textContent
+    );
+
+    const expectedOptions = ['매월 17일', '매월 3번째 토요일'];
+
+    expectedOptions.forEach((option) => {
+      expect(repeatRuleOptions).toContain(option);
+    });
+  });
+
+  it('그외 날짜에 매년 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매년 o월 o일, 해당 연도의 o월에서 o일이 속한 주차의 같은 요일', async () => {
+    const { user } = setup(<App />);
+
+    await user.type(screen.getByLabelText('날짜'), '2024-02-17');
+
+    const checkbox = screen.getByLabelText('반복 일정') as HTMLInputElement;
+    if (!checkbox.checked) {
+      await user.click(checkbox);
+    }
+
+    const repeatTypeSelect = screen.getByLabelText('반복 유형');
+    await user.selectOptions(repeatTypeSelect, '매년');
+
+    const repeatRuleSelect = (await screen.findByLabelText('반복 규칙')) as HTMLSelectElement;
+    const repeatRuleOptions = Array.from(repeatRuleSelect.options).map(
+      (option) => option.textContent
+    );
+
+    const expectedOptions = ['매년 2월 17일', '매년 2월 3번째 토요일'];
+
+    expectedOptions.forEach((option) => {
+      expect(repeatRuleOptions).toContain(option);
+    });
+  });
 });
