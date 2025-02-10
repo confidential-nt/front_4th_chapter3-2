a807652c34d62d92284285b4a4292f2de04b673c
confidential-nt <youyoy689@gmail.com>
2025-02-10 18:18:29 +0900

    test: 테스트 작성
    - 사용자는 일정 생성 또는 수정 시 반복 유형을 선택할 수 있다.
    - 반복 유형은 매일, 매주, 매월, 매년 중 하나여야 한다.

---
+++ src/__tests__/repeat.spec.tsx
@@ -0,0 +1,46 @@
+import { screen } from '@testing-library/react';
+
+import App from '../App';
+import { setup } from './rtl-utils';
+
+describe.only('반복 유형 선택', () => {
+  it('사용자는 일정 생성 또는 수정 시 반복 유형을 선택할 수 있다.', async () => {
+    const { user } = setup(<App />);
+
+    const checkbox = screen.getByLabelText('반복 일정') as HTMLInputElement;
+
+    if (!checkbox.checked) {
+      await user.click(checkbox);
+    }
+
+    const expected = screen.getByLabelText('반복 유형');
+    expect(expected).toBeInTheDocument();
+  });
+
+  it('반복 유형은 매일, 매주, 매월, 매년 중 하나여야 한다.', () => {
+    setup(<App />);
+
+    const select = screen.getByLabelText('반복 유형') as HTMLSelectElement;
+    const options = Array.from(select.options).map((option) => option.textContent);
+
+    const expectedOptions = ['매일', '매주', '매월', '매년'];
+
+    expectedOptions.forEach((option) => {
+      expect(options).toContain(option);
+    });
+  });
+
+  it('윤년의 2월 29일에 매월 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매월 29일, 해당 월에서 29일이 속한 주차의 같은 요일, 해당 월의 마지막 동일 요일, 해당 월의 마지막 날.', () => {
+    const { user } = setup(<App />);
+  });
+
+  it('윤년의 2월 29일에 매년 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매년 2월 29일, 해당 연도의 2월에서 29일이 속한 주차의 같은 요일, 해당 연도의 2월 마지막 동일 요일, 해당 연도의 2월 마지막 날.', () => {});
+
+  it('31일에 매월 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매월 31일, 해당 월에서 31일이 속한 주차의 같은 요일, 해당 월의 마지막 동일 요일, 해당 월의 마지막 날.', () => {});
+
+  it('31일에 매년 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매년 동일 월의 31일, 해당 연도의 동일 월에서 31일이 속한 주차의 같은 요일, 해당 연도의 동일 월 마지막 동일 요일, 해당 연도의 동일 월 마지막 날.', () => {});
+
+  it('그외 날짜에 매월 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매월 o일, 해당 월에서 o일이 속한 주차의 같은 요일', () => {});
+
+  it('그외 날짜에 매년 반복 설정 시, 다음 옵션 중 하나를 선택할 수 있다: 매년 o월 o일, 해당 연도의 o월에서 o일이 속한 주차의 같은 요일', () => {});
+});
